c40e6d8c2f9b64be553a472680cdabe0e465112d
pritiranjan1605 <pritiranjansamal868@gmail.com>
2024-02-06 00:27:32 +0530

    rooms features added

--- app.js
+++ app.js
@@ -7,18 +7,18 @@ dotenv.config();
 const jwt = require('jsonwebtoken')
 const secret = "vGuWUZSBgk"
 
-function setuser(user){
+function setuser(user) {
     const payload = {
         // name:user.name,
-        email:user.email,
-        password:user.password
+        email: user.email,
+        password: user.password
     }
-    const token = jwt.sign(payload,secret)
+    const token = jwt.sign(payload, secret)
     return token;
 }
 
-function getuser(token1){
-    const token = jwt.verify(token1,secret)
+function getuser(token1) {
+    const token = jwt.verify(token1, secret)
     return token;
 }
 
@@ -26,84 +26,118 @@ function getuser(token1){
 
 const cookie = require('cookie-parser')
 const User = require('./models/user')
-const {v4:uuidv4} =require('uuid')
+const { v4: uuidv4 } = require('uuid')
 const mongoose = require('mongoose')
 const express = require('express')
 const path = require('path')
 const http = require('http')
-const {Server} =require("socket.io");
+const { Server } = require("socket.io");
 const cookieParser = require('cookie-parser');
 const app = express();
 
 
 
 
-mongoose.connect("mongodb://127.0.0.1:27017/chatlyy").then(()=>console.log("connected")).catch((err)=>console.log(err))
 
 
-app.set("view engine","ejs")
-app.set("views",path.resolve('./views'))
+
+app.set("view engine", "ejs")
+app.set("views", path.resolve('./views'))
 const myserver = http.createServer(app)
 const io = new Server(myserver)
-io.on('connection',(socket)=>{
+io.on('connection', (socket) => {
+
+
     console.log("socket connected")
     console.log(socket.id)
-    socket.on("message",(data)=>{
-        io.sockets.emit("message",{message:data,id:socket.id})
+    socket.on("joinroom", (data) => {
+        const roomno = data.roomno;
+        const message = data.message;
+        if (roomno.trim() !== "") {
+            socket.join(roomno)
+            io.to(roomno).emit("message", { message: message, id: socket.id })
+        }
+        else {
+            io.sockets.emit("message", { message: message, id: socket.id });
+        }
+
     })
 
-    socket.on("disconnect",()=>{
+    socket.on("exitRoom", (data) => {
+        const roomno = data.roomno;
+        if (roomno && roomno.trim() !== "") {
+            socket.leave(roomno);
+            console.log(`User ${socket.id} left room ${roomno}`);
+        }
+    });
+
+    socket.on("message", (data) => {
+        const roomno = data.roomno;
+        const message = data.message;
+        if (roomno && roomno.trim() !== "") {
+            io.to(roomno).emit("message", { message: message, id: socket.id });
+        } else {
+            io.sockets.emit("message", { message: message, id: socket.id });
+        }
+
+    })
+
+
+    socket.on("disconnect", () => {
         console.log("socket disconnected")
     })
+
+
+
 })
 app.use(express.static('public'))
-app.use(express.urlencoded({extended:false}))
+app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
-app.post('/signup',async (req,res)=>{
-    const {name ,email,password} = req.body;
-    if ( !name || !email || !password) {
+app.post('/signup', async (req, res) => {
+    const { name, email, password } = req.body;
+    if (!name || !email || !password) {
         console.log("nothing is present")
         return res.render('signup')
     }
     const user = await User.create({
-        name,email,password
+        name, email, password
     })
-    
+
     return res.render("login")
 
 })
 
-app.post('/login',async (req,res)=>{
-    const {email,password} = req.body;
+app.post('/login', async (req, res) => {
+    const { email, password } = req.body;
     if (!req.body) {
         console.log("nothing is present")
         return res.render('login')
     }
-    console.log(email,password)
-    const user  = await User.findOne({email,password})
-    if(!user){
+    console.log(email, password)
+    const user = await User.findOne({ email, password })
+    if (!user) {
         console.log("no such user found")
-        return res.render('login',{
-            error:"incorrect email or password"
+        return res.render('login', {
+            error: "incorrect email or password"
         })
     }
-    const token = setuser(user,secret)
-    res.cookie('token',token)
+    const token = setuser(user, secret)
+    res.cookie('token', token)
 
-    return res.render('index') 
+    return res.render('index')
 })
 
 
 
 
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     res.render('login')
 })
-app.get('/signup',(req,res)=>{
+app.get('/signup', (req, res) => {
     res.render('signup')
 })
-mongoose.connect(process.env.MONGO_URL).then(()=>console.log("connected")).catch((err)=>console.log(err))
+mongoose.connect(process.env.MONGO_URL).then(() => console.log("connected")).catch((err) => console.log(err))
 const port = process.env.PORT || 8000;
-myserver.listen(port,()=>{
+myserver.listen(port, () => {
     console.log("hello server connected")
 })
